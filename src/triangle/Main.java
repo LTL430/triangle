2a8e1ceb6af713b9239b70ad777fae55b1246132
LTL430 <lifengwu430@gmail.com>
2026-02-19 20:47:40 -0500

    v3.0

--- src/triangle/Main.java
+++ src/triangle/Main.java
@@ -6,30 +6,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.print("please input a:");
-		double a=sc.nextDouble();
+		double a = sc.nextDouble();
 		System.out.print("please input b:");
-		double b=sc.nextDouble();
+		double b = sc.nextDouble();
 		System.out.print("please input c :");
-		double c=sc.nextDouble();
-	
-		if (a+b<=c) {
-			System.out.print("no1");
-			return;
-			
+		double c = sc.nextDouble();
+		if ((a==90 || b==90 || c==90) && a+b+c==180) {
+			System.out.print("recteangle");
 		}
-		if (b+c<=a) {
-			System.out.print("no2");
-			return;
-			
+		if (a+b+c==180 && a!=90 && b!=90 && c!=90) {
+			System.out.print("triangle");
 		}
-		if (c+a<=b) {
-			System.out.print("no3");
-			return;
-			
+		if (a+b+c!=180) {
+			System.out.print("error");
 		}
-		System.out.print("yes");
 		
 	}
 
